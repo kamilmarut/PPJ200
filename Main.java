b7850b21d22dd72cb42e7a2efe323f1e3088b982
kamilmarut <mkamiruu@gmail.com>
2020-01-31 09:08:04 +0100

    Test command

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        for (int i = 0; i < 6; i++) {
+            System.out.println(i);
+        }
     }
 }
